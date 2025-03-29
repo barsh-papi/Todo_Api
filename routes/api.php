7902fd0bd5f6742a2f3217b615f2647ef41b9ac0
barsh-papi <paabart0@gmail.com>
2025-03-29 03:21:16 -1200

    3rd

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/add', [TodoController::class, 'add']);
+Route::post('/add', [TodoController::class, 'add']);
